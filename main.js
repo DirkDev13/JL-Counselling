b3e5f5ea92d58c1c1a5cfd627356e0fdebfa83af
DirkDev <dirkdklerk@gmail.com>
2026-04-08 12:59:39 +0200

    add scroll functions to about services and contact sections

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 function scrollToBooking() {
     document.getElementById("booking").scrollIntoView({ behavior: "smooth" });
 }
+function scrollToAbout() {
+    document.getElementById("about").scrollIntoView({ behavior: "smooth" });
+}
+function scrollToSupport() {
+    document.getElementById("support").scrollIntoView({ behavior: "smooth" });
+}
+function scrollToSocial() {
+    document.getElementById("social").scrollIntoView({ behavior: "smooth" });
+}
