3ef6a927946d2fb91494247c88f0947ee04e7a58
DirkDev <dirkdklerk@gmail.com>
2026-06-13 09:33:06 +0200

    add animation to scroll through the different testimonials

--- main.js
+++ main.js
@@ -16,3 +16,20 @@ function scrollToTop(){
         behavior: 'smooth'
     });
 }
+
+//testimonials animation
+const track = document.querySelector(".testimonials-track");
+const pillars = document.querySelectorAll(".testimonials-pillar");
+const visibleCount = 3;
+let index = 0;
+
+function moveTestimonials() {
+  index++;
+  if (index > pillars.length - visibleCount) {
+    index = 0; // reset to start
+  }
+  const offset = -(index * (100 / visibleCount));
+  track.style.transform = `translateX(${offset}%)`;
+}
+
+setInterval(moveTestimonials, 8000); // every 6 seconds
