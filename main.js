832fe03248781a10fbc956258b43cd5a0f1b9333
DirkDev <dirkdklerk@gmail.com>
2026-06-25 11:25:38 +0200

    remove the areas of support and change the booking into types of bookings

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ function scrollToBooking() {
 function scrollToAbout() {
     document.getElementById("about").scrollIntoView({ behavior: "smooth" });
 }
-function scrollToSupport() {
-    document.getElementById("support").scrollIntoView({ behavior: "smooth" });
-}
+
 function scrollToContact() {
     document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
 }
